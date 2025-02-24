4876987e497d43b7eaa98198a0437002e0405af6
杜可 <161901040+dukerki@users.noreply.github.com>
2025-02-24 10:49:42 +0800

    删除不再使用的文件和脚本，添加新的 URL 列表

---
+++ create.py
@@ -0,0 +1,65 @@
+#之前师哥通过从网页上爬去比赛链接，大部分挂题的同学都不能根据要求起名字
+#并且这种方法受到密码，外界因素（如其他同学挂出来了名字类似的题目，以及太早的比赛爬不下来）
+#现在改成手动输入比赛链接，只爬all_url.txt里面的比赛
+import run
+from selenium import webdriver
+#爬取所有比赛
+def get_all():
+    
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument("window-size=1024,768")
+    chrome_options.add_experimental_option(
+        "excludeSwitches", ['enable-automation', 'enable-logging'])
+    chrome_options.add_argument("--no-sandbox")
+    browser = webdriver.Chrome(options=chrome_options)
+    browser.set_page_load_timeout(30)  # 设置页面加载超时时间为60秒
+
+    with open("all_url.txt", "r", encoding="UTF-8") as f:
+        for line in f:
+            line = line.rstrip("\n")
+            f = open("url.txt", 'w',encoding="UTF-8").close()  # 先清空文件
+            f = open("url.txt", 'w',encoding="UTF-8")
+            print(f'cur_url: {line}')
+            f.write(f'{line}#rank\n')
+            f.write(f'{line}#rank\n') 
+            f.close()
+            while True:
+                try:
+                    run.Crawl_and_save(browser)
+                    # print(f"done: {line}")
+                    break
+                except Exception as e:
+                    print(e,"retry")
+
+#只爬取url的比赛
+def get_lastest(url):
+    
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument("window-size=1024,768")
+    chrome_options.add_experimental_option(
+        "excludeSwitches", ['enable-automation', 'enable-logging'])
+    chrome_options.add_argument("--no-sandbox")
+    browser = webdriver.Chrome(options=chrome_options)
+    browser.set_page_load_timeout(30)  # 设置页面加载超时时间为60秒
+
+    f = open("url.txt", 'w',encoding="UTF-8").close()  # 先清空文件
+    f = open("url.txt", 'w',encoding="UTF-8")
+    f.write(f'{url}#rank\n')
+    f.write(f'{url}#rank\n') 
+    f.close()
+    #这里加入了重新申请机制，防止网络问题引起中途报错
+    while True:
+        try:
+            run.Crawl_and_save(browser)
+            # print(f"done: {line}")
+            break
+        except Exception as e:
+            print(e,"retry")
+
+if __name__ == "__main__":
+    # get_all()
+    get_lastest("https://vjudge.net/contest/695643")
